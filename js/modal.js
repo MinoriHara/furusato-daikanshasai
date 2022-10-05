208c39f7f2edde1406faed38bb0aa48b6ca018b7
MinoriHara <113651991+MinoriHara@users.noreply.github.com>
2022-10-06 07:53:37 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,10 @@ modalBtns.forEach(function (btn) {
   btn.onclick = function () {
     var modal = btn.getAttribute('data-modal');
     document.getElementById(modal).style.display = "block";
+    const selector = document.querySelector(".home");
+    selector.classList.add("modal_open");
+    const scroll = document.getElementById(modal).querySelector(".modal-content");
+    scroll.classList.add("modal_scroll");
   };
 });
 const closeBtns = document.querySelectorAll(".modal-close");
@@ -10,11 +14,20 @@ closeBtns.forEach(function (btn) {
   btn.onclick = function () {
     var modal = btn.closest('.modal');
     modal.style.display = "none";
+    const selector = document.querySelector(".home");
+    selector.classList.remove("modal_open");
+    const scroll = event.target.querySelector(".modal-content");
+    scroll.classList.remove("modal_scroll");
+    document.getElementsByClassName("modal-content").scrollTop = 0;
   };
 });
-
 window.onclick = function (event) {
   if (event.target.className === "modal") {
     event.target.style.display = "none";
+    const selector = document.querySelector(".home");
+    selector.classList.remove("modal_open");
+    const scroll = event.target.querySelector(".modal-content");
+    scroll.classList.remove("modal_scroll");
+    document.getElementsByClassName("modal-content").scrollTop = 0;
   }
 };
